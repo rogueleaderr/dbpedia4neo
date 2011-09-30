e0184f50e93407878ead0e9d048434619ca9560c
Peter Neubauer <peter@neubauer.se>
2011-09-30 15:01:48 +0200

    fixing the dependencies

--- src/test/java/org/tinkerpop/rdf/ImportNTTest.java
+++ src/test/java/org/tinkerpop/rdf/ImportNTTest.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.acaro.dbpedia4neo.inserter.DBpediaLoader;
 import org.junit.Test;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
@@ -14,6 +13,10 @@ import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.impl.EmptyBindingSet;
 import org.openrdf.query.parser.ParsedQuery;
 import org.openrdf.query.parser.sparql.SPARQLParser;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.Sail;
@@ -32,23 +35,31 @@ public class ImportNTTest
         neo.setMaxBufferSize( 5000 );
         Sail sail = new GraphSail( neo );
         sail.initialize();
+        SailRepositoryConnection connection = new SailRepository(sail).getConnection();
         System.out.println( "initialized" );
-        // loadTriples( neo, sail );
-        String queryString = ""
-                             + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-                             + "PREFIX rdfs:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-                             + "PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> "
-                             + "SELECT ?product ?label" + "WHERE { "
-                             + "?product rdfs:label ?label ."
-                             + "?product rdf:type bsbm:Product ."
-                             + "FILTER regex(?label, 'r')}";
+//        loadTriples( neo, connection );
+        String queryString1 = "" +
+        		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+        		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        		"PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> " +
+        		"SELECT ?product ?label " +
+        		"WHERE {" +
+        		"    ?product rdfs:label ?label ." +
+        		"    ?product rdf:type bsbm:Product ." +
+        		"    FILTER regex(?label, \"r\")}";
+        String queryString2 = "" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX rdfs:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> " +
+                "SELECT ?product " +
+                "WHERE { ?product rdf:type bsbm:Product .} ";
         SPARQLParser parser = new SPARQLParser();
         ParsedQuery query = null;
         CloseableIteration<? extends BindingSet, QueryEvaluationException> sparqlResults;
 
         try
         {
-            query = parser.parseQuery( queryString,
+            query = parser.parseQuery( queryString1,
                     "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/" );
         }
         catch ( MalformedQueryException e )
@@ -79,14 +90,21 @@ public class ImportNTTest
 
     }
 
-    private void loadTriples( Neo4jGraph neo, Sail sail )
+    private void loadTriples( Neo4jGraph neo, SailRepositoryConnection connection )
             throws RDFParseException, RDFHandlerException,
             FileNotFoundException, IOException, SailException
     {
         File file = new File( "berlin_nt_100.nt" );
         System.out.println( "Loading " + file + ": " );
-        DBpediaLoader.loadFile( file.getPath(), sail.getConnection(),
-                sail.getValueFactory() );
+        try
+        {
+            connection.add(file, null, RDFFormat.NTRIPLES);
+        }
+        catch ( RepositoryException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         System.out.print( '\n' );
     }
 }
